3b39e3de05dcdc2701ea54505f676d3c6150df02
chen parnasa <chenparnasa@gmail.com>
2021-11-11 15:24:13 +0200

    added chameleon project page
    update from11.11.21

--- js/script.js
+++ js/script.js
@@ -68,6 +68,77 @@ scrollDown.forEach((link)=>{
 
 //---------------- Mouse Cursor ----------------//
 
+// Copy email
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+
+
+
+/**
+ * Change the text of a button
+ * @param {el} object HTMLElement: button to change text of
+ * @param {dText} string: default text
+ * @param {nText} string: new text
+ **/
+ function changeText(el, dText, nText) {
+    var content = '',
+        context = '';
+    
+    /**
+     * Set the text of a button
+     *     - dependant upon current text
+     **/
+    function setText() {
+      return (content === dText) ? nText : dText;
+    }
+    
+    /* Check to see if the browser accepts textContent */
+    if ('textContent' in document.body) {
+      context = 'textContent';
+      /* Get the current button text */
+      content = el[context];
+    /* Browser does NOT use textContent */
+    } else {
+      /* Get the button text with fallback option */
+      content = el.firstChild.nodeValue;
+    }
+    
+    /* Set button text */
+    if (context === 'textContent') {
+      el.textContent = setText();
+    } else {
+      el.firstChild.nodeValue = setText();
+    }
+  }
+  
+  var defaultText,
+      substituteText,
+      btn;
+  
+  /* Default text of the button */
+  defaultText = 'Email';
+  /* Alternate text for button */
+  substituteText = 'Copied!';
+  /* Grab our button */
+  btn = document.querySelector('.btn');
+  
+  /* Add a listener to the button instance so we can manipulate it */
+  btn.addEventListener('click', function() {
+    changeText(this, defaultText, substituteText);
+  }, false);
+  
+
+
+
+   
+
 
 /*
 //---------------- Mobile Nav ----------------//
